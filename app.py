d9e3875fa9186df90fff386648f60a3edb72221a
Nikita Malafeyev <nikitamalafeev95@gmail.com>
2026-04-26 15:36:27 +0300

    add admin password

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, session, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
